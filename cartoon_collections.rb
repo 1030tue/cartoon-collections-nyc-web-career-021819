4ec5bba1907410bdadcadd15be6287d66a67c750
Kate <katekim5809@gmail.com>
2019-02-04 20:06:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,6 +26,7 @@ def find_the_cheese(food)
   # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each.with_index do 
   
   
   
